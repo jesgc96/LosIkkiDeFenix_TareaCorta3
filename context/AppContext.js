7c61b963b48721eaf1f45aa3311780de2c2994e0
Geral <geraldpacheco11@gmail.com>
2026-04-07 23:20:55 -0600

    Persona 4

--- context/AppContext.js
+++ context/AppContext.js
@@ -4,19 +4,41 @@ export const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
 
-  const [datos, setDatos] = useState([
-    { id: '1', campo1: 'Dato A', campo2: 'Valor X', campo3: 'Info 1' },
-    { id: '2', campo1: 'Dato B', campo2: 'Valor Y', campo3: 'Info 2' },
-    { id: '3', campo1: 'Dato C', campo2: 'Valor Z', campo3: 'Info 3' },
-  ]);
-
+  const [datos, setDatos] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // Simulación de carga (luego Persona 4 mete API)
-    setTimeout(() => {
-      setLoading(false);
-    }, 1000);
+    // Consumo de la API Pública de MicroPersonas (Responsabilidad de Persona 4)
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://tiusr13pl.cuc-carrera-ti.ac.cr/MicroPersonas/api/Persona');
+        const json = await response.json();
+        
+        // Mapeo de datos para que calcen con la interfaz de la Persona 2
+        const datosMapeados = json.map(persona => ({
+          id: persona.personaId.toString(),
+          campo1: persona.nombre,
+          campo2: "Tipo: " + persona.tipo,
+          campo3: "Género: " + persona.gender
+        }));
+
+        setDatos(datosMapeados);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+        
+        // Si hay error en la conexión (como el bloqueo CORS del navegador Web), mandamos una tarjeta de advertencia
+        setDatos([{
+          id: 'error-999',
+          campo1: '¡Error de conexión! (CORS)',
+          campo2: 'Intenta probarlo en tu celular',
+          campo3: 'con la App de Expo Go'
+        }]);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
   }, []);
 
   return (
